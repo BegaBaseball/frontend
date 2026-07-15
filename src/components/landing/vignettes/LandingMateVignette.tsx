72a15fe7ab83002aa17edd322e9cb3600ac29b31
rkdansdlf <ansdlf000616e@gmail.com>
2026-07-16 01:29:38 +0900

    fix: strengthen 랜딩 feature story 계약

--- src/components/landing/vignettes/LandingMateVignette.tsx
+++ src/components/landing/vignettes/LandingMateVignette.tsx
@@ -6,10 +6,10 @@ export default function LandingMateVignette() {
     <div className="landing-mate-vignette">
       <article className="landing-vignette-card landing-mate-card">
         <header>
-          <span className="landing-mate-matchup">
+          <div className="landing-mate-matchup">
             <img src={TEAM_ASSETS[LANDING_MATE_DATA.team]} alt="" width={24} height={24} />
             <h3>{LANDING_MATE_DATA.matchup}</h3>
-          </span>
+          </div>
           <span className="landing-mate-status">{LANDING_MATE_DATA.status}</span>
         </header>
         <ul className="landing-mate-details">
